edefa270b57f5ae66c59519280e7f8282f35f3b2
KimSiTek <sihuk38@gachon.ac.kr>
2025-04-05 17:24:50 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/map" element={<MapPage />} />
+        <Route path="/" element={<MapPage />} />
 
       </Routes>
     </BrowserRouter>
