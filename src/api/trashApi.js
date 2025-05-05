102b604d6ecf73584d7284f16c660fd498e16409
KimSiTek <sihuk38@gachon.ac.kr>
2025-05-05 20:13:50 +0900

    Update trashApi.js

--- src/api/trashApi.js
+++ src/api/trashApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://trashmap-backend-production.up.railway.app/';
+const BASE_URL = 'https://trashmap-backend-production.up.railway.app/api/trashbins';
 
 export async function fetchTrashBins() {
   try {
