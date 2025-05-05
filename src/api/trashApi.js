cfdc587bcf8888bdb46506cd6071c3001789f24e
KimSiTek <sihuk38@gachon.ac.kr>
2025-05-05 16:48:17 +0900

    Update trashApi.js

--- src/api/trashApi.js
+++ src/api/trashApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://trash-map-eta.vercel.app/map?id=tancheon01';
 
 export async function fetchTrashBins() {
   try {
