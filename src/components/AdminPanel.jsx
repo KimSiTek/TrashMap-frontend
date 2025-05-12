f030ff809737fcacba95dbd2dc50728dea8a0e23
KimSiTek <sihuk38@gachon.ac.kr>
2025-05-13 08:30:20 +0900

    Update AdminPanel.jsx

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -18,31 +18,31 @@ export default function AdminPanel() {
       <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
 
         {images.map((item, idx) => {
-           console.log("🧾 서버 응답 항목:", item); // ✅ 여기!
-  const filename = item.imagePath.replace(/^\/?uploads\//, "");
-  const imageUrl = `https://trashmap-backend-production.up.railway.app/api/files/${filename}`;
-  return (
-    <div key={idx} style={{ width: "220px" }}>
-      <img
-        src={imageUrl}
-        alt={`신고사진-${idx}`}
-        style={{
-          width: "100%",
-          height: "auto",
-          border: "1px solid #ccc",
-          borderRadius: "8px"
-        }}
-      />
-      <p style={{ fontSize: "14px", margin: "6px 0 2px 0" }}>
-        🗑️ <strong>{item.trashBinName}</strong>
-      </p>
-      <p style={{ fontSize: "13px", color: "#555" }}>
-        ⏰ {item.uploadedAt}
-      </p>
+          console.log("🧾 서버 응답 항목:", item); // ✅ 여기!
+          const filename = item.imagePath.replace(/^\/?uploads\//, "");
+          const imageUrl = `https://trashmap-backend-production.up.railway.app/api/files/${filename}`;
+          return (
+            <div key={idx} style={{ width: "220px" }}>
+              <img
+                src={imageUrl}
+                alt={`신고사진-${idx}`}
+                style={{
+                width: "100%",
+                height: "auto",
+                border: "1px solid #ccc",
+                borderRadius: "8px"
+                }}
+              />
+              <p style={{ fontSize: "14px", margin: "6px 0 2px 0" }}>
+                🗑️ <strong>{item.trashBinName}</strong>
+              </p>
+              <p style={{ fontSize: "13px", color: "#555" }}>
+                ⏰ {item.uploadedAt}
+              </p>
 
-    </div>
-  );
-})}
+            </div>
+          );
+        })}
 
       </div>
     </div>
