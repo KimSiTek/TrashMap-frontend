a801430c72ec515dd8ee0472c5d65a2601ba2fb9
KimSiTek <sihuk38@gachon.ac.kr>
2025-05-12 14:02:27 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MapPage from './pages/MapPage';
 import HomePage from './pages/HomePage';
+import AdminPanel from './components/AdminPanel';
 
 function App() {
   return (
